01c2d87bc907ff7ac97304d65d42b9ad7a50b713
Andy Li <zhengjunli36@gmail.com>
2017-09-27 16:28:02 -0400

    Added job 1 - build NGram library.

--- src/main/java/NGramLibraryBuilder.java
+++ src/main/java/NGramLibraryBuilder.java
@@ -14,22 +14,41 @@ public class NGramLibraryBuilder {
 		int noGram;
 		@Override
 		public void setup(Context context) {
-			//how to get n-gram from command line?
+			// how to get n-gram from command line?
+			Configuration configuration = context.getConfiguration();
+			// If we get nothing from command line as "noGram", the default value is 5.
+			noGram = configuration.getInt("noGram", 5);
 		}
 
 		// map method
 		@Override
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			
-			String line = value.toString();
-			
-			line = line.trim().toLowerCase();
 
-			//how to remove useless elements?
-			
-			//how to separate word by space?
-			
-			//how to build n-gram based on array of words?
+			// Because in Driver.java, we have ' conf1.set("textinputformat.record.delimiter", ".") ',
+			// So we read data by sentence instead of by line.
+			String sentence = value.toString();
+			sentence = sentence.trim().toLowerCase();
+			// how to remove useless elements?
+			sentence.replaceAll("[^a-z]", " ");
+
+			// how to separate word by space?
+			// split by ' ', '\t', ..., etc
+			String[] words = sentence.split("\\s+");
+			// Get rid of 1-gram
+			if (words.length < 2) {
+				return;
+			}
+
+			// how to build n-gram based on array of words?
+			for (int i = 0; i < words.length; i++) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(words[i]);
+				for (int j = 1; i + j < words.length && j < noGram; j++) {
+					sb.append(" ");
+					sb.append(words[i + j]);
+					context.write(new Text(sb.toString().trim()), new IntWritable(1));
+				}
+			}
 		}
 	}
 
@@ -39,7 +58,12 @@ public class NGramLibraryBuilder {
 		public void reduce(Text key, Iterable<IntWritable> values, Context context)
 				throws IOException, InterruptedException {
 
-			//how to sum up the total count for each n-gram?
+			// how to sum up the total count for each n-gram?
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new IntWritable(sum));
 		}
 	}
 
